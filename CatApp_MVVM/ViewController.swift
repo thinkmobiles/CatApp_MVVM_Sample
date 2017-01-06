21a53953e257af4c293cbc98df4e348d3bfb3781
Sauron Black <sauronblack@Saurons-MacBook-Pro.local>
2017-01-06 13:14:46 +0200

    Initial Commit

---
+++ CatApp_MVVM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CatApp_MVVM
+//
+//  Created by Sauron Black on 1/6/17.
+//  Copyright © 2017 Sauron Black. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
